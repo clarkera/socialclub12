bb517ecf25676e9315a6f0e102cd053451bab2ac
clarkera <rclarke9941@gmail.com>
2024-07-13 14:53:46 -0400

    update conection.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/socialclub1",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/socialclub12",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
